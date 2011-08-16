0000648314fc80df68f25aa6863c66a963ca0cb6
dellis1972 <dellis1972@googlemail.com>
2011-08-16 12:26:47 +0100

    Added Windows Platformer port

---
+++ StarterKits/Windows/Platformer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Platformer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (PlatformerGame game = new PlatformerGame())
+                {
+                    game.Run();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
