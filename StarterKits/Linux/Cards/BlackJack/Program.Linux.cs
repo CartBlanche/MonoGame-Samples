d9c28de817e0b21e2cbb59f42d6d699fd38b050b
Dominique Louis <dominique@dominique-VirtualBox.(none)>
2011-10-19 13:02:44 +0100

    Cards StarterKit ported to Linux. Graphics work. Sound untested as not able to get OpenAL working.

---
+++ StarterKits/Linux/Cards/BlackJack/Program.Linux.cs
@@ -0,0 +1,33 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// Program.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using System;
+using System.IO;
+
+#endregion
+
+namespace Blackjack
+{
+	class Program
+	{
+		private static BlackjackGame game;
+		
+		/// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+		{
+			game = new BlackjackGame();
+			game.Run ();
+		}
+	}
+}
+
