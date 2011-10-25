40e1e8967399a2454879ab22322ef730f61da66a
CartBlanche <Dominique@SavageSoftwareSolutions.com>
2011-10-25 14:05:37 +0100

    VideoPlayer sample for MacOS X

---
+++ Samples/MacOS/VideoPlayer/Game1.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+
+#if ANDROID
+using Android.App;
+#endif
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Microsoft.Xna.Samples.Draw2D
+{
+	/// <summary>
+	/// This is the main type for your game
+	/// </summary>
+	public class Game1 : Microsoft.Xna.Framework.Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;		
+		Texture2D texture, ball;
+		SpriteFont font;
+		float size, rotation;
+		float clippingSize = 0.0f;
+		Color alphaColor = Color.White;
+		Video video;
+		VideoPlayer videoPlayer;
+		bool playVideo = false;
+		
+        public Game1 ()  
+		{
+			graphics = new GraphicsDeviceManager (this);
+			
+			Content.RootDirectory = "Content";
+			
+			graphics.PreferMultiSampling = true;
+			graphics.IsFullScreen = true;	
+
+			graphics.SupportedOrientations = DisplayOrientation.Portrait | DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight | DisplayOrientation.PortraitUpsideDown;
+		}
+		
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize ()
+		{
+			// TODO: Add your initialization logic here
+
+			base.Initialize ();
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent ()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch (GraphicsDevice);
+
+			// TODO: use this.Content to load your game content here
+			font = Content.Load<SpriteFont> ("spriteFont1");
+			
+			video = Content.Load<Video> ("Jackie Brown-Trailer");
+			videoPlayer = new VideoPlayer(this);
+			playVideo = true;
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update (GameTime gameTime)
+		{
+			// TODO: Add your update logic here
+			if (playVideo)
+			{
+				if (videoPlayer.State == MediaState.Stopped) 
+				{
+					videoPlayer.Play(video);	
+				}
+			}
+
+			base.Update (gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw (GameTime gameTime)
+		{
+			graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
+			
+			// Won't be visible until we hide the movie
+			spriteBatch.Begin();
+			spriteBatch.DrawString(font, "Video Player Test", new Vector2 (50, 40), Color.Red);
+			spriteBatch.End();
+		}
+	}
+}
