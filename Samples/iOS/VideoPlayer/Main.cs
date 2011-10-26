4bf72b93c9b82bc97fbb576e0d1c3294fc5b412b
CartBlanche <Dominique@SavageSoftwareSolutions.com>
2011-10-26 22:59:45 +0100

    Use m4v format and default to landscape mode.

--- Samples/iOS/VideoPlayer/Main.cs
+++ Samples/iOS/VideoPlayer/Main.cs
@@ -6,13 +6,22 @@ using MonoTouch.UIKit;
 
 namespace MonoGame.Samples.VideoPlayer
 {
-	public class Application
+	[Register("AppDelegate")]
+	class Program : UIApplicationDelegate
 	{
-		// This is the main entry point of the application.
+		private Game1 game;
+
+		public override void FinishedLaunching (UIApplication app)
+		{
+			game = new Game1 ();
+			game.Run ();
+		}
+
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
 		static void Main (string[] args)
 		{
-			// if you want to use a different Application Delegate class from "AppDelegate"
-			// you can specify it here.
 			UIApplication.Main (args, null, "AppDelegate");
 		}
 	}
