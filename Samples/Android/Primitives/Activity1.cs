ad08c2ed00777b6c816b14c651cdf8fd9fd9fabf
CartBlanche <Dominique@SavageSoftwareSolutions.com>
2011-11-13 14:09:59 +0000

    Android port of Primitives Sample.

---
+++ Samples/Android/Primitives/Activity1.cs
@@ -0,0 +1,26 @@
+using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace MonoGame.Samples.Primitives.Android
+{
+	[Activity (Label = "MonoGame.Samples.Primitives.Android", MainLauncher = true)]
+	public class Activity1 : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Create our OpenGL view, and display it
+			GLView1 view = new GLView1 (this);
+			SetContentView (view);
+		}
+	}
+}
+
+
