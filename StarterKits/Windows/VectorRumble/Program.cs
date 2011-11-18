08de9c11fad106a8aef436bd6446f988e9ffb9f8
dellis1972 <dellis1972@googlemail.com>
2011-11-18 16:40:28 +0000

    Added VectorRumble for windows (not working yet)

---
+++ StarterKits/Windows/VectorRumble/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VectorRumble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (VectorRumbleGame game = new VectorRumbleGame())
+            {
+                game.Run();
+            }
+
+        }
+    }
+}
