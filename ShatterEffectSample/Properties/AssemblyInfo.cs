db3232652a1cbb6ca82841985241055ac695c8c2
espes <nielkie2@gmail.com>
2012-06-17 02:16:37 +1000

    Add ShatterEffectSample sample

---
+++ ShatterEffectSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Shatter Effect")]
+[assembly: AssemblyProduct("Shatter Effect")]
+[assembly: AssemblyDescription("This sample shows how you can apply an effect on any model in your game to shatter it apart. The effect is simulated with a vertex shader.")]
+[assembly: AssemblyCompany("Microsoft")]
+
+[assembly: AssemblyCopyright("Copyright © Microsoft")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
