02cc3bd182683a444444b430ff1d82cc748810f4
CartBlanche <savagesoftware@gmail.com>
2024-09-19 12:48:41 +1000

    Remove deprecated xml and use API call.

--- GameStateManagement/GameStateManagement.WindowsDX/Program.cs
+++ GameStateManagement/GameStateManagement.WindowsDX/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 using GameStateManagement.Core;
+using System.Windows.Forms;
 
+Application.SetHighDpiMode(HighDpiMode.SystemAware);  // Adjust the mode as needed
 using var game = new GameStateManagementGame();
 game.Run();
