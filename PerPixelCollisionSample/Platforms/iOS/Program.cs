de75699f4b01c1250b3a0547b7722d83909814f5
CartBlanche <savagesoftware@gmail.com>
2025-07-23 10:37:42 +0100

    PerPixelCollition updated to SDK and MG 3.8.*

---
+++ PerPixelCollisionSample/Platforms/iOS/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using UIKit;
+
+namespace PerPixelCollision.iOS
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, typeof(AppDelegate));
+        }
+    }
+}
