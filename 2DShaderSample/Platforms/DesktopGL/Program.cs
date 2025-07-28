b8fb87ea63f701e5049d0f21646e9010f4c838ef
CartBlanche <savagesoftware@gmail.com>
2025-07-29 00:19:44 +0100

    2DShaderSample updated to SDK and MG 3.8.*

---
+++ 2DShaderSample/Platforms/DesktopGL/Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace ShaderTest.DesktopGL
+{
+    /// <summary>
+    /// The main entry point for the DesktopGL application.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Application entry point.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new ShaderTestGame())
+                game.Run();
+        }
+    }
+}
