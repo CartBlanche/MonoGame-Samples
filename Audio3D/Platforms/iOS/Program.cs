55014c4dca610e0df637fd29e4211ff9f00524e8
Dominique Louis <savagesoftware@gmail.com>
2025-08-04 20:34:37 +0100

    Audio3D - Changes so iOS builds.

--- Audio3D/Platforms/iOS/Program.cs
+++ Audio3D/Platforms/iOS/Program.cs
@@ -1,9 +1,19 @@
+using Foundation;
 using UIKit;
 
 namespace Audio3D.iOS
 {
-    public class Application
+    [Register("AppDelegate")]
+    class AppDelegate : UIApplicationDelegate
     {
+        private Audio3DGame game;
+
+        public override void FinishedLaunching(UIApplication app)
+        {
+            game = new Audio3DGame();
+            game.Run();
+        }
+
         static void Main(string[] args)
         {
             UIApplication.Main(args, null, typeof(AppDelegate));
