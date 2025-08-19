bd91d1883c19960715a290710d6cc6fc79f636c4
Dominique Louis <savagesoftware@gmail.com>
2025-08-19 12:51:38 +0100

    [NetworkStateManagement] - Get Android Building and use Content pipeline.

--- NetworkStateManagement/Platforms/Android/MainActivity.cs
+++ NetworkStateManagement/Platforms/Android/MainActivity.cs
@@ -1,19 +1,36 @@
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
+
 using Microsoft.Xna.Framework;
 
 namespace NetworkStateManagement
 {
-    [Activity(Label = "NetworkStateManagement", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(
+        Label = "@string/app_name",
+        MainLauncher = true,
+        Icon = "@drawable/icon",
+        AlwaysRetainTaskState = true,
+        LaunchMode = LaunchMode.SingleInstance,
+        ScreenOrientation = ScreenOrientation.SensorLandscape,
+        ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden
+    )]
     public class MainActivity : AndroidGameActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        private NetworkStateManagementGame _game;
+        private View _view;
+
+        protected override void OnCreate(Bundle bundle)
         {
-            base.OnCreate(savedInstanceState);
-            var game = new NetworkStateManagementGame();
-            SetContentView((game.Services.GetService(typeof(View)) as View));
-            game.Run();
+            base.OnCreate(bundle);
+
+            _game = new NetworkStateManagementGame();
+
+            _view = _game.Services.GetService(typeof(View)) as View;
+            SetContentView(_view);
+
+            _game.Run();
         }
     }
 }
